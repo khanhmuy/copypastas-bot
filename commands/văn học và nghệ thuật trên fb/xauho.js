c5b889a8278478992fcdd17f5ca9ed10708a1693
khanhmuy <maikhanhhuy0608@outlook.com>
2022-02-27 08:18:37 +0700

    stuff

---
+++ commands/văn học và nghệ thuật trên fb/xauho.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'xauho',
+    description: 'Thật xấu hổ khi đây là bạn của bạn',
+    cooldown: 1,
+    async execute(client, message) {
+        message.delete();
+        message.channel.send('Lúc sáng mình có xem được 1 vid phát tán trên mạng xh. Thật xấu hổ khi phải thú nhận rằng đây là bạn của mình, cũng là người trong vid này. thì lúc ấy là bạn ý có những giây phút thiếu suy nghĩ và bồng bột và thật không ngờ rằng những hành động đó bị người khác quay lại và đã bị lan truyền trên MXH. Chủ tus có biết rằng bạn ấy đã khóc rất nhiều không?? bạn ấy đã rất hối hận với những việc mình đã làm, đến nỗi bạn ý còn không dám bước chân ra khỏi nhà hay là nhìn mặt ai,bạn đã mất ăn mất ngủ. Thậm chí là cả ngày điên điên dại dại, đến mức lên cơn bênh chó, cắn người nữa ạ! vì thế nên mong bạn chủ tus có thể xoá vid đó giúp mình vớii ạ !')
+    }
+}
