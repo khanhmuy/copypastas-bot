6d55875c1d8a6f7a9d787e9dd54c230577d0b5c7
khanhmuy <maikhanhhuy0608@outlook.com>
2022-03-03 08:21:44 +0700

    man why

--- events/triggers.js
+++ events/triggers.js
@@ -38,7 +38,7 @@ module.exports = {
                 ]
             }
             cry.cry.forEach(word => {
-                if (message.content.includes(khóc)) {
+                if (message.content.includes(word)) {
                 message.reply({files: ['https://cdn.discordapp.com/attachments/942078114715037716/948550275722006578/45A468AF-8528-4E2E-9F0A-02B777F1A691.mov'], allowedMentions: { repliedUser: false }})
                 }
             })
